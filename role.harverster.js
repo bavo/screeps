be08b162bfd881d5ddca2a569fdcb28b10bdba5a
bavo <bavomees@gmail.com>
2018-01-17 11:38:05 +0100

    add role upgrader

--- role.harverster.js
+++ role.harverster.js
@@ -1,13 +1,22 @@
 var roleHarvester = {
     run: function (creep) {
-        if (creep.carryCapacity < creep.carry.energy) {
+
+        if (creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.harvesting = false;
+            creep.say('Transferring ...');
+        }
+        if (!creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
+            creep.say('Harvesting ...');
+        }
+        if (creep.memory.harvesting) {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {}});
             }
         } else {
             if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+                creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {}});
             }
         }
     }
