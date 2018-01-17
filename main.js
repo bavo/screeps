a5babf303f99705a59d8a7ca1c3cfc0c8baa067a
bavo <bavomees@gmail.com>
2018-01-17 11:19:32 +0100

    add harverster functionality

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+var roleHarvester = require('role.harverster');
+
 module.exports.loop = function () {
-    var creep = Game.creeps['Harvester1'];
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
     }
-}
+};
